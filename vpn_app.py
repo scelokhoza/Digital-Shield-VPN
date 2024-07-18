988678edcff0d27ff4d34cc20c52334650c2c19d
skhoza023 <skhoza023@student.wethinkcode.co.za>
2024-07-18 14:41:05 +0200

    adding a basic page where the user will find a button to start the vpn client to initiate connection to the vpn server

--- vpn_app.py
+++ vpn_app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+import subprocess
+from flask import Flask, render_template, request, jsonify
 from client.vpn_client import VPNClient
 
 
 app = Flask(__name__)
 
-vpn_client = VPNClient()
+vpn_client = VPNClient('config.toml')
 
 
 @app.route('/')
@@ -12,15 +13,18 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/get-started', methods=['POST'])
+def vpn_page():
+    return render_template('vpn.html')
 
-# @app.route('/proxy', methods=['POST'])
-# def proxy():
-#     if request.method=='POST':
-#         url = request.form['url']
-#         response = vpn_client.fetch_url(url)
-#         return response
-    
+
+@app.route('/start-vpn', methods=['POST'])
+def start_vpn():
+    vpn_client.connect_to_vpn()
+    # return jsonify({'success': True})
+    # else:
+    #     return jsonify({'success': False}), 500
+
 
 if __name__ == '__main__':
-    # vpn_client.connect_to_vpn()
     app.run(debug=True)
