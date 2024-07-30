c1c7e9e138aacc4426dd9666d64ac68318a7ee78
skhoza023 <skhoza023@student.wethinkcode.co.za>
2024-07-30 12:04:47 +0200

    updating the server and client for looging to check if the connection is successful and data can be exchanged between the two

--- client/vpn_client.py
+++ client/vpn_client.py
@@ -2,12 +2,14 @@ import socket
 import toml
 import ssl
 import threading
+import logging
 from dataclasses import dataclass
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.fernet import Fernet
 
-
+# Setup logging
+logging.basicConfig(level=logging.INFO)
 
 @dataclass
 class VPNData:
@@ -15,17 +17,15 @@ class VPNData:
     server_hostname: str
     port: int
     local_port: int
-    
 
 class Configuration:
     def __init__(self, target_file: str) -> None:
         self.file = target_file
-        
-    
+
     def load_config_data(self) -> VPNData:
         with open(self.file, 'r') as config_file:
             config_data = toml.load(config_file)
-        
+
         return VPNData(
             server_address=config_data['server']['server_address'],
             server_hostname=config_data['server']['server_hostname'],
@@ -33,13 +33,9 @@ class Configuration:
             local_port=config_data['server']['local_port']
         )
 
-
-
 class VPNClient:
-    
     client_config: Configuration
-    
-    
+
     def __init__(self, config_file: str):
         self.client_config = Configuration(config_file)
         self.configuration: VPNData = self.client_config.load_config_data()
@@ -50,7 +46,6 @@ class VPNClient:
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.context = ssl.create_default_context()
 
-        # Disable hostname verification and certificate verification for self-signed certificates
         self.context.check_hostname = False
         self.context.verify_mode = ssl.CERT_NONE
 
@@ -58,17 +53,14 @@ class VPNClient:
         self.symmetric_key = Fernet.generate_key()
         self.cipher = Fernet(self.symmetric_key)
 
-
     def connect_to_vpn(self):
         try:
             self.secure_socket.connect((self.server_address, self.port))
-            print(f"Connected to VPN server at {self.server_address}:{self.port}")
+            logging.info(f"Connected to VPN server at {self.server_address}:{self.port}")
 
-            # Receive server's public key
             server_public_key_pem = self.secure_socket.recv(4096)
             server_public_key = serialization.load_pem_public_key(server_public_key_pem)
 
-            # Encrypt the symmetric key with the server's public key and send it
             encrypted_symmetric_key = server_public_key.encrypt(
                 self.symmetric_key,
                 padding.OAEP(
@@ -79,15 +71,16 @@ class VPNClient:
             )
             self.secure_socket.sendall(encrypted_symmetric_key)
 
-            # Start local proxy to forward traffic through the VPN
             self.start_local_proxy()
-        
+
         except ssl.SSLError as e:
-            print(f"SSL error: {e}")
+            logging.error(f"SSL error: {e}")
             raise
         except Exception as e:
-            print(f"An error occurred: {e}")
+            logging.error(f"An error occurred: {e}")
             raise
+       
+
         finally:
             self.secure_socket.close()
 
@@ -95,7 +88,7 @@ class VPNClient:
         local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         local_socket.bind(('127.0.0.1', self.local_port))
         local_socket.listen(5)
-        print(f"Local proxy started on port {self.local_port}")
+        logging.info(f"Local proxy started on port {self.local_port}")
 
         while True:
             client_conn, client_addr = local_socket.accept()
@@ -105,9 +98,9 @@ class VPNClient:
         try:
             while True:
                 data = client_conn.recv(4096)
-                print(data)
                 if not data:
                     break
+                logging.info(f"Data sent to VPN server: {len(data)} bytes")
                 encrypted_data = self.cipher.encrypt(data)
                 self.secure_socket.sendall(encrypted_data)
 
@@ -123,11 +116,13 @@ class VPNClient:
                 encrypted_response = b''.join(response_chunks)
                 response = self.cipher.decrypt(encrypted_response)
                 client_conn.sendall(response)
+                logging.info(f"Data received from VPN server: {len(response)} bytes")
 
         except Exception as e:
-            print(f"An error occurred: {e}")
+            logging.error(f"An error occurred: {e}")
         finally:
             client_conn.close()
+
 
 
 if __name__ == "__main__":
--- server/vpn_server.py
+++ server/vpn_server.py
@@ -1,53 +1,37 @@
 import ssl
 import socket
 import threading
+import logging
 from urllib.parse import urlparse
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.fernet import Fernet
 
+# Setup logging
+logging.basicConfig(level=logging.INFO)
 
 class VPNServer:
     def __init__(self, server_address='0.0.0.0', port=8080, certfile='server.crt', keyfile='server.key'):
-        """
-        Initializes the VPNServer with the given server address, port, certificate, and key files.
-        Generates RSA keys for secure communication.
-        
-        Parameters:
-        server_address (str): IP address to bind the server to.
-        port (int): Port to listen on.
-        certfile (str): Path to the SSL certificate file.
-        keyfile (str): Path to the SSL key file.
-        """
         self.server_address = server_address
         self.port = port
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.certfile = certfile
         self.keyfile = keyfile
-        self.clients = {}  # Dictionary to store client addresses and their threads
-        self.client_traffic = {}  # Dictionary to store client IPs and their data usage
+        self.clients = {}
+        self.client_traffic = {}
 
-        # Generate RSA keys for the server
         self.private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
         self.public_key = self.private_key.public_key()
-
-        # Serialize public key to send to the client
         self.public_pem = self.public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         )
 
-
     def start_vpn(self):
-        """
-        Starts the VPN server, binding to the specified address and port.
-        Listens for incoming connections and spawns a new thread to handle each client.
-        Also starts a thread to handle admin commands.
-        """
         try:
             self.server_socket.bind((self.server_address, self.port))
             self.server_socket.listen(5)
-            print(f"Server started on {self.server_address}:{self.port}")
+            logging.info(f"Server started on {self.server_address}:{self.port}")
 
             threading.Thread(target=self.admin_commands).start()
 
@@ -55,41 +39,27 @@ class VPNServer:
                 client_socket, client_address = self.server_socket.accept()
                 self.clients[client_address] = client_socket
                 self.client_traffic[client_address[0]] = 0
-                print(f"Connection from {client_address}")
+                logging.info(f"Connection from {client_address}")
                 client_thread = threading.Thread(target=self.handle_client, args=(client_socket, client_address))
                 client_thread.start()
 
         except PermissionError as e:
-            print(f"Permission denied: {e}")
+            logging.error(f"Permission denied: {e}")
         except Exception as e:
-            print(f"An error occurred: {e}")
+            logging.error(f"An error occurred: {e}")
         finally:
             self.server_socket.close()
 
-
     def handle_client(self, client_socket, client_address):
-        """
-        Handles communication with a connected client.
-        Performs SSL handshake, exchanges encryption keys, and forwards data to the destination server.
-        
-        Parameters:
-        client_socket (socket.socket): The socket connected to the client.
-        client_address (tuple): The client's address.
-        """
         try:
-            # Set socket timeout to handle idle connections
-            client_socket.settimeout(60)
-            
+            client_socket.settimeout(100)
             ssl_client_socket = ssl.wrap_socket(client_socket,
                                                 server_side=True,
                                                 certfile=self.certfile,
                                                 keyfile=self.keyfile,
                                                 ssl_version=ssl.PROTOCOL_TLS)
             
-            # Send the server's public key to the client
             ssl_client_socket.sendall(self.public_pem)
-
-            # Receive the encrypted symmetric key from the client
             encrypted_symmetric_key = ssl_client_socket.recv(4096)
             symmetric_key = self.private_key.decrypt(
                 encrypted_symmetric_key,
@@ -106,44 +76,35 @@ class VPNServer:
                     encrypted_data = ssl_client_socket.recv(4096)
                     if not encrypted_data:
                         break
-
                     data = cipher.decrypt(encrypted_data)
                     self.client_traffic[client_address[0]] += len(data)
+                    logging.info(f"Data received from {client_address}: {len(data)} bytes")
+
                     response = self.forward_to_destination(data)
                     encrypted_response = cipher.encrypt(response)
                     ssl_client_socket.sendall(encrypted_response)
                 except socket.timeout:
-                    print("Socket timed out. Closing connection.")
+                    logging.warning("Socket timed out. Closing connection.")
                     break
                 except ssl.SSLError as e:
-                    print(f"SSL error: {e}")
+                    logging.error(f"SSL error: {e}")
                     break
                 except Exception as e:
-                    print(f"An error occurred while handling client data: {e}")
+                    logging.error(f"An error occurred while handling client data: {e}")
                     break
 
         except ssl.SSLError as e:
-            print(f"SSL error during initial handshake: {e}")
+            logging.error(f"SSL error during initial handshake: {e}")
         except socket.timeout:
-            print("Initial socket connection timed out. Closing connection.")
+            logging.warning("Initial socket connection timed out. Closing connection.")
         except Exception as e:
-            print(f"An error occurred during client handling: {e}")
+            logging.error(f"An error occurred during client handling: {e}")
         finally:
             ssl_client_socket.close()
             client_socket.close()
             del self.clients[client_address]
 
-
     def forward_to_destination(self, data):
-        """
-        Forwards client data to the intended destination server and returns the response.
-        
-        Parameters:
-        data (bytes): The data received from the client to be forwarded.
-        
-        Returns:
-        bytes: The response data from the destination server.
-        """
         try:
             headers = data.split(b'\r\n')
             host_header = next((h for h in headers if b'Host:' in h), None)
@@ -151,9 +112,7 @@ class VPNServer:
                 raise ValueError("No Host header found")
             
             host = host_header.split(b' ')[1].decode('utf-8')
-            
             url = urlparse(f'http://{host}')
-            
             port = url.port or 80
             if port == 443:
                 url = urlparse(f'https://{host}')
@@ -179,14 +138,10 @@ class VPNServer:
             return response_data
         
         except Exception as e:
-            print(f"Error forwarding data to destination: {e}")
+            logging.error(f"Error forwarding data to destination: {e}")
             return b""
 
-
     def admin_commands(self):
-        """
-        Handles admin commands to manage and troubleshoot the VPN server.
-        """
         while True:
             command = input("Enter command: ").strip().lower()
             if command == "list clients":
@@ -201,61 +156,38 @@ class VPNServer:
                 self.quit_server()
                 break
             else:
-                print("Unknown command")
-
+                logging.warning("Unknown command")
 
     def list_clients(self):
-        """
-        Lists all connected clients.
-        """
         if self.clients:
-            print("Connected clients:")
+            logging.info("Connected clients:")
             for client_address in self.clients.keys():
-                print(client_address)
+                logging.info(client_address)
         else:
-            print("No connected clients.")
-
+            logging.info("No connected clients.")
 
     def show_traffic(self, client_ip):
-        """
-        Shows the traffic data for a specific client.
-        
-        Parameters:
-        client_ip (str): The IP address of the client.
-        """
         traffic = self.client_traffic.get(client_ip, None)
         if traffic is not None:
-            print(f"Traffic for {client_ip}: {traffic} bytes")
+            logging.info(f"Traffic for {client_ip}: {traffic} bytes")
         else:
-            print(f"No traffic data for {client_ip}")
-
+            logging.warning(f"No traffic data for {client_ip}")
 
     def disconnect_client(self, client_ip):
-        """
-        Disconnects a specific client.
-        
-        Parameters:
-        client_ip (str): The IP address of the client.
-        """
         for client_address, client_socket in list(self.clients.items()):
             if client_address[0] == client_ip:
                 client_socket.close()
                 del self.clients[client_address]
-                print(f"Disconnected client {client_ip}")
+                logging.info(f"Disconnected client {client_ip}")
                 return
-        print(f"No client found with IP {client_ip}")
-
+        logging.warning(f"No client found with IP {client_ip}")
 
     def quit_server(self):
-        """
-        Shuts down the server and disconnects all clients.
-        """
-        print("Shutting down server...")
+        logging.info("Shutting down server...")
         for client_socket in self.clients.values():
             client_socket.close()
         self.server_socket.close()
-        print("Server shut down.")
-
+        logging.info("Server shut down.")
 
 
 if __name__ == '__main__':
